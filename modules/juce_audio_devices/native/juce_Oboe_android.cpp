070746257f2d8fe2f5a0f53bdb2df04db8506f44
Eric_Lian <git@superexercisebook.com>
2025-11-25 17:59:16 +0800

    Milody Patch: Exposes Oboe stream state

--- modules/juce_audio_devices/native/juce_Oboe_android.cpp
+++ modules/juce_audio_devices/native/juce_Oboe_android.cpp
@@ -264,6 +264,8 @@ public:
     String getLastError() override                      { return lastError; }
     bool isPlaying() override                           { return callback.get() != nullptr; }
     int getXRunCount() const noexcept override          { return session->getXRunCount(); }
+    int getAudioInputStreamState() const noexcept override    { return session->getInputStreamState(); }
+    int getAudioOutputStreamState() const noexcept override   { return session->getOutputStreamState(); }
 
     int getDefaultBufferSize() override
     {
@@ -542,6 +544,16 @@ private:
             return 0;
         }
 
+        int getState() const
+        {
+            if (stream != nullptr)
+            {
+                return (int)stream->getState();
+            }
+
+            return 0;
+        }
+
     private:
         void open (int deviceId, oboe::Direction direction,
                    oboe::SharingMode sharingMode,
@@ -658,6 +670,26 @@ private:
             int outputXRunCount = jmax (0, outputStream != nullptr ? outputStream->getXRunCount() : 0);
 
             return inputXRunCount + outputXRunCount;
+        }
+
+        int getOutputStreamState() const
+        {
+            if (outputStream)
+            {
+                return outputStream->getState();
+            }
+
+            return 0;
+        }
+
+        int getInputStreamState() const
+        {
+            if (inputStream)
+            {
+                return inputStream->getState();
+            }
+
+            return 0;
         }
 
     protected:
@@ -987,7 +1019,6 @@ private:
 
             openStreams();
             start();
-            owner.onOutputStreamStarted(outputStream->getNativeStream());
         }
 
         void destroyStreams()
